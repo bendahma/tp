6af664bd901f3c5efbd5c82e478e5b20103d8430
BENDJEDDOU AYMEN <BENDJEDDOUAYMEN@gmail.com>
2022-07-23 02:45:26 +0200

    dashboard changed

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
    <x-slot name="header">
-       {{ __('Dashboard') }}
+      {{ __('Dashboard') }}
    </x-slot>
 
-   <div class="p-4 bg-white rounded-lg shadow-xs">
-       {{ __('lang.loggedIn') }}
-   </div>
 </x-app-layout>
